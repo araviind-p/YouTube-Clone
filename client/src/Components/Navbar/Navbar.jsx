fae1421e863c95c99cfee26a539075f32dabced4
Aravind <aravindparavind10@gmail.com>
2024-05-14 14:03:12 +0530

    NavBar

---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "./logo.ico";
+import SearchBar from "./SearchBar/SearchBar";
+import { RiVideoAddLine } from "react-icons/ri";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { BiUserCircle } from "react-icons/bi";
+
+function Navbar() {
+  // const CurrentUser = null;
+  const CurrentUser = {
+    result: {
+      email: "xyz@BiLogoGmail.com",
+      joinedOn: "2/10/2024",
+    },
+  };
+  return (
+    <div className="Container_Navbar">
+      <div className="Burger_Logo_Navbar">
+        <div className="burger">
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+        <div className="logo_div_Navbar">
+          <img src={logo} alt="logo" className="main_logo" />
+          <p className="logo_title_navbar">YouTube</p>
+        </div>
+      </div>
+      <SearchBar />
+      <RiVideoAddLine size={22} className={"vid_bell_Navbar"} />
+      <div className="apps_Box">
+        <p className="appBox"></p>
+        <p className="appBox"></p>
+        <p className="appBox"></p>
+        <p className="appBox"></p>
+        <p className="appBox"></p>
+        <p className="appBox"></p>
+        <p className="appBox"></p>
+        <p className="appBox"></p>
+        <p className="appBox"></p>
+      </div>
+      <IoMdNotificationsOutline size={22} className="vid_bell_Navbar" />
+      <div className="Auth_cont_Navbar">
+        {CurrentUser ? (
+          <>
+            <div className="Channel_logo_App">
+              <p className="fstChar_logo_App">
+                {CurrentUser?.result.name ? (
+                  <>{CurrentUser?.result.name.charAt(0).toUpperCase()}</>
+                ) : (
+                  <>{CurrentUser?.result.email.charAt(0).toUpperCase()}</>
+                )}
+              </p>
+            </div>
+          </>
+        ) : (
+          <>
+            <p className="Auth_Btn">
+              <BiUserCircle size={22} />
+              <b>Sign in</b>
+            </p>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
