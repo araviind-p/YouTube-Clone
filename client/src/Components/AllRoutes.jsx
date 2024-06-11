88d5fa5e53058e65fb339e500ef12a944258e84c
Aravind <aravindparavind10@gmail.com>
2024-06-11 20:15:42 +0530

    video page

--- client/src/Components/AllRoutes.jsx
+++ client/src/Components/AllRoutes.jsx
@@ -6,17 +6,19 @@ import YourVideo from "./../Pages/YourVideo/YourVideo";
 import WatchHistory from "./../Pages/WatchHistory/WatchHistory";
 import WatchLater from "./../Pages/WatchLater/WatchLater";
 import LikedVideo from "./../Pages/LikedVideo/LikedVideo";
+import VideoPage from "../Pages/VideoPage/VideoPage";
 
 function AllRoutes() {
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/library" element={<Library />} />
-        <Route path="/history" element={<WatchHistory />} />
-        <Route path="/watchlater" element={<WatchLater />} />
-        <Route path="/likedvideo" element={<LikedVideo />} />
-        <Route path="/yourvideos" element={<YourVideo />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/library" element={<Library />} />
+      <Route path="/history" element={<WatchHistory />} />
+      <Route path="/watchlater" element={<WatchLater />} />
+      <Route path="/likedvideo" element={<LikedVideo />} />
+      <Route path="/yourvideos" element={<YourVideo />} />
+      <Route path="/videopage/:vid" element={<VideoPage />} />
+    </Routes>
   );
 }
 
---
+++ client/src/Pages/VideoPage/VideoPage.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import vid from "../../Components/Video/vid.mp4";
+import "./VideoPage.css";
+
+function VideoPage() {
+  return (
+    <>
+      <div className="container_videoPage">
+        <div className="container2_videoPage">
+          <div className="video_display_screen_videoPage">
+            <video
+              src={vid}
+              className="video_ShowVideo_videoPage"
+              controls
+              autoPlay
+            ></video>
+            <div className="video_details_videoPage">
+              <div className="video_btns_title_VideoPage_cont">
+                <p className="video_title_VideoPage">Title</p>
+                <div className="views_date_btns_VideoPage">
+                  <div className="views_videoPage">
+                    5 views <div className="dot"></div> Uploaded 1 year ago
+                  </div>
+                </div>
+              </div>
+              <div className="channel_details_videoPage">
+                <b className="channel_logo_videoPage">
+                  <p>C</p>
+                </b>
+                <p className="channel_name_videoPage">Channel name</p>
+              </div>
+              <div className="comments_VideoPage">
+                <h2>
+                  <ul>comments</ul>
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className="moreVideoBar">More video</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default VideoPage;
